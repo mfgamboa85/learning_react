3b7b144d687dd6f3363d904feca4a837a5add2da
mfg <mfgamboa85@gmail.com>
2025-03-06 11:06:06 -0500

    worked on styling and disabling of product cards

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice'
 
 function ProductList({ onHomeClick }) {
+    const cart = useSelector(state => state.cart.items)
     const dispatch = useDispatch()
     const [addedToCart, setAddedToCart] = useState({})
     const [showCart, setShowCart] = useState(false);
@@ -238,32 +239,6 @@ function ProductList({ onHomeClick }) {
         textDecoration: 'none',
     }
 
-    const productGridStyle = {
-        width: '100%',
-        maxWidth: '600px',
-        margin: '30px 0'
-    }
-
-    const productListItemStyle = {
-        width: '100%',
-        margin: '60px 0',
-        padding: '20px',
-        border: '1px solid darkgray'
-    }
-
-    const productListItemImageStyle = {
-        objectFit: 'contain',
-        maxHeight: '200px',
-        width: '100%'
-    }
-
-    const productListItemButtonStyle = {
-        margin: '10px 0',
-        padding: '10px',
-        fontSize: '18px',
-        cursor: 'pointer'
-    }
-
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
@@ -292,6 +267,11 @@ function ProductList({ onHomeClick }) {
         }))
     }
 
+    const getTotalItems = () => {
+        const qty = cart.reduce((total, item) => total + item.quantity, 0)
+        return qty
+    }
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -308,23 +288,36 @@ function ProductList({ onHomeClick }) {
 
                 </div>
                 <div style={styleObjUl}>
-                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div>
+                        <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a>
+                    </div>
+                    <div>
+                        <a className="cart-link" href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                            <h1 className='cart'>
+                                <span className="cart_quantity_count">{getTotalItems()}</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>
+                            </h1>
+                        </a>
+                    </div>
                 </div>
             </div>
             {!showCart ? (
                 <div className="product-grid">
                     { plantsArray.map(set => (
-                    <div className={set.category} style={productGridStyle}>
+                    <div className={set.category}>
                         <h2>{set.category}</h2>
-                        <ul>
+                        <ul className="product-list">
                             { set.plants.map(plant => (
-                            <li style={productListItemStyle}>
-                                <h3>{plant.name}</h3>
-                                <img src={plant.image} alt={plant.name} style={productListItemImageStyle} />
+                            <li className="product-card">
+                                <h3 className="plantname_heading">{plant.name}</h3>
+                                <img className="product-image" src={plant.image} alt={plant.name} />
                                 <p style={{ padding: '10px 0' }}>{plant.description}</p>
                                 <span style={{ display: 'block' }}>{plant.cost}</span>
-                                <button style={productListItemButtonStyle} onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                <button
+                                    className={cart.find(added => added.name === plant.name) ? "product-button added-to-cart" : "product-button"}
+                                    onClick={() => handleAddToCart(plant)}
+                                    {...(cart.find(added => added.name === plant.name) && { disabled: true })}
+                                >Add to Cart</button>
                             </li>
                             )) }
                         </ul>
